332513021d598c7b46ac1615ab706b421abd0754
Kiptim <kiptim54@gmail.com>
2018-05-10 11:20:56 +0300

    set up authentication

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from . import views
 
+
 urlpatterns = [
    url(r'^$', views.home_page, name='index'),
    url(r'^profile/', views.user_profile, name='profile'),
+   url(r'^logout/', views.logout, {"next_page": '/'}),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import logout
+from django.contrib.auth.decorators import login_required
 
 def home_page(request):
     title = "Instagram | Home"
     return render(request, 'user/index.html', {"title": title})
-
+@login_required
 def user_profile(request):
     title="Instagram | Home "
-    return render(request, 'user/index.html', {"title":title})
+    return render(request, 'user/profile.html', {"title":title})
