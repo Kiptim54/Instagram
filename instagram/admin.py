71f4c54b165751107b426eae1b4173615c808a82
Kiptim <kiptim54@gmail.com>
2018-05-10 13:26:57 +0300

    add database

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Photos
+from .models import Photos, Profile
 
 admin.site.register(Photos)
+admin.site.register(Profile)
--- instagram/models.py
+++ instagram/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    profile_photo=models.ImageField(upload_to='article/')
+    bio=models.CharField(max_length=100)
+    user=models.ForeignKey(User, on_delete=models.CASCADE )
+
+
 class Photos(models.Model):
     image=models.ImageField(upload_to='articles/')
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    image_name=models.CharField(blank=True, max_length=50)
+    image_caption=models.CharField(blank=True,max_length=100)
+    profile=models.ForeignKey(Profile)
+   
 
     @classmethod
     def display_images(cls):
